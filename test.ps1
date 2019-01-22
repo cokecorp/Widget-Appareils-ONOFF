b6f2ffbf3a39e199374975a76a2eab1b8d3f4149
cokecorp <sbouthet@gmail.com>
2019-01-22 11:24:53 +0100

    suppression de fichier et netoyage

--- test.ps1
+++
@@ -1,6 +0,0 @@
-﻿$name = (get-content 'C:\Users\XWSC7030\Documents\GitHub\Widget-Appareils-ONOFF\liste2.txt')
-$file = 'C:\Users\XWSC7030\Documents\GitHub\Widget-Appareils-ONOFF\liste2-modifier.txt'
-
-foreach ($NewName in $name) {
-ADD-content -path $file -value '"100" $NewName "200" $NewName "300" $NewName "400" $NewName "500" $NewName "600" $NewName "800" $NewName'
-}
