358a6ebfd6bad3cb5456fe263d5fc947e0095e24
cokecorp <sbouthet@gmail.com>
2019-01-22 11:10:03 +0100

    modif icon

---
+++ test.ps1
@@ -0,0 +1,6 @@
+﻿$name = (get-content 'C:\Users\XWSC7030\Documents\GitHub\Widget-Appareils-ONOFF\liste2.txt')
+$file = 'C:\Users\XWSC7030\Documents\GitHub\Widget-Appareils-ONOFF\liste2-modifier.txt'
+
+foreach ($NewName in $name) {
+ADD-content -path $file -value '"100" $NewName "200" $NewName "300" $NewName "400" $NewName "500" $NewName "600" $NewName "800" $NewName'
+}
